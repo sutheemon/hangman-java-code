3cda1a9e53da55d749874b17c902bc9fbffdfd84
Sutheemon <50098265+sutheemon@users.noreply.github.com>
2019-05-07 14:30:42 +0700

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,5 +1,4 @@
-package Server;
-
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -7,30 +6,26 @@ import java.lang.ClassNotFoundException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Random;
+import java.util.Scanner;
 
-/**
- * This class implements java Socket server
- * @author pankaj
- *
- */
 public class Server {
-    
+
     //static ServerSocket variable
     private static ServerSocket server;
     //socket server port on which it will listen
-    private static int port = 9876;
-    
+    private static int port = 8765;
+
     public static void main(String args[]) throws ClassNotFoundException {
         //create the socket server object
-        
+
         //keep listens indefinitely until receives 'exit' call or program terminates
         int i =0;
         Thread thread = null;
         try {
             server = new ServerSocket(port);
-
+			System.out.println("Waiting for the client request");
             while(true){
-                System.out.println("Waiting for the client request");
+                
                 Socket socket = server.accept();
                 ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
                 String message = (String) ois.readObject();
@@ -47,7 +42,6 @@ public class Server {
                 oos.close();
                 socket.close();
                 i++;
-
             }
         } catch (IOException ex) {
             try {
@@ -57,5 +51,5 @@ public class Server {
             }
         }
     }
-    
+
 }
